e38acbb18dcfd9d9793e5c9205097dbb36f812f4
Дамир <110933288+mealet@users.noreply.github.com>
2024-07-14 20:04:10 +0300

    Update and rename install.sh to hiw-install.sh

--- hiw-install.sh
+++ hiw-install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if command -v rustup &> /dev/null
-then
-    echo "rustup is already installed"
-else
-    echo "Installing rustup..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-fi
+echo "Installing rustup..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> $HOME/.bashrc
 echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> $HOME/.zshrc
